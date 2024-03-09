97bbbcc949517aee3c8429fdc6e9ac7a1088d1a6
Maximilian Rågmo <max.ragmo@outlook.com>
2024-03-09 20:20:08 +0100

    Update index.js

--- program/melodifestivalen/2024/index.js
+++ program/melodifestivalen/2024/index.js
@@ -15,11 +15,11 @@ game.contestant("Cazzi Opeia");
 game.contestant("Jacqline").bet("OR").bet("JR",false);
 game.contestant("Liamoo");
 game.contestant("Maria Sur");
-game.contestant("Smash Into Pieces");
+game.contestant("Smash Into Pieces").bet("MS",false);
 game.contestant("Danny Saucedo").bet("LR",false).bet("AR",false);
 game.contestant("Dotter");
-game.contestant("Jay Smith").bet("RS",false);
-game.contestant("Annika Wickihalder").bet("RS").bet("MR");
+game.contestant("Jay Smith").bet("RS",false).bet("MS").bet("MS",false);
+game.contestant("Annika Wickihalder").bet("RS").bet("MR").bet("MS");
 game.contestant("Marcus & Martinus").bet("JR").bet("LR").bet("AR").bet("OR",false);
 game.contestant("Medina").bet("MR",false);
 

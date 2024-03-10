5d72e4112a7ff211d1e3922058bc18cb159b4c83
Maximilian Rågmo <max.ragmo@outlook.com>
2024-03-10 09:25:09 +0100

    Update index.js

--- program/melodifestivalen/2024/index.js
+++ program/melodifestivalen/2024/index.js
@@ -10,17 +10,17 @@ const game = new Game(
   ["melodifestivalen", "rounded", "theme-white"],
   ["🪩", "🎈", "🎤"]
 );
-game.contestant("Lisa Ajax");
-game.contestant("Cazzi Opeia");
-game.contestant("Jacqline").bet("OR").bet("JR",false);
-game.contestant("Liamoo");
-game.contestant("Maria Sur");
-game.contestant("Smash Into Pieces").bet("MS",false);
-game.contestant("Danny Saucedo").bet("LR",false).bet("AR",false);
-game.contestant("Dotter");
-game.contestant("Jay Smith").bet("RS",false).bet("MS").bet("MS",false);
-game.contestant("Annika Wickihalder").bet("RS").bet("MR").bet("MS");
-game.contestant("Marcus & Martinus").bet("JR").bet("LR").bet("AR").bet("OR",false);
-game.contestant("Medina").bet("MR",false);
+game.contestant("Lisa Ajax").eliminate();
+game.contestant("Cazzi Opeia").eliminate();
+game.contestant("Jacqline").bet("OR").bet("JR",false).eliminate();
+game.contestant("Liamoo").eliminate();
+game.contestant("Maria Sur").eliminate();
+game.contestant("Smash Into Pieces").bet("MS",false).eliminate();
+game.contestant("Danny Saucedo").bet("LR",false).bet("AR",false).eliminate();
+game.contestant("Dotter").eliminate();
+game.contestant("Jay Smith").bet("RS",false).bet("MS").bet("MS",false).eliminate();
+game.contestant("Annika Wickihalder").bet("RS").bet("MR").bet("MS").eliminate();
+game.contestant("Marcus & Martinus").bet("JR").bet("LR").bet("AR").bet("OR",false).winner();
+game.contestant("Medina").bet("MR",false).eliminate();
 
 export { game };

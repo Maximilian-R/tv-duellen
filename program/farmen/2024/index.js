dbd54e4685102b2a736b3ed9624df28c47f8f7e9
Maximilian <max.ragmo@outlook.com>
2024-03-17 20:29:32 +0100

    Update index.js

--- program/farmen/2024/index.js
+++ program/farmen/2024/index.js
@@ -7,7 +7,7 @@ const game = new Game(
 );
 game.contestant("Maxine").eliminate().bet("AR").bet("OR");
 game.contestant("Ciro").eliminate().bet("JR");
-game.contestant("Tim").bet("RS");
+game.contestant("Tim").eliminate().bet("RS");
 game.contestant("Desirée S").eliminate().bet("MR");
 game
   .contestant("Magnus")
@@ -19,12 +19,12 @@ game.contestant("Cornelia G").eliminate().bet("AR", false);
 game.contestant("Jennifer").eliminate().bet("RS", false);
 game.contestant("Andreas").eliminate();
 game.contestant("Per R").eliminate();
-game.contestant("Fredrik");
+game.contestant("Fredrik").eliminate();
 game.contestant("Anna").eliminate();
 game.contestant("Martina").quit();
 game.contestant("Ronja").eliminate();
-game.contestant("Cornelia N");
-game.contestant("Levin");
+game.contestant("Cornelia N").eliminate();
+game.contestant("Levin").win();
 game.contestant("Yorgo").quit();
 game.contestant("Alessia").eliminate();
 game.contestant("Desirée N").quit();

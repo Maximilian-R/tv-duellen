6b5e49a194b27e2e4817b32c9c5be4d6aad84ed6
Maximilian Rågmo <max.ragmo@outlook.com>
2024-10-13 17:33:57 +0200

    Groupby polyfill for github actions env

--- eleventy/index.11ty.js
+++ eleventy/index.11ty.js
@@ -6,6 +6,25 @@ import {
 } from "../library/views.js";
 import { Leaderboard } from "../library/game.js";
 
+const hasGroup =
+  typeof Object.groupBy === typeof undefined ||
+  typeof Array.groupToMap === typeof undefined ||
+  typeof Array.group === typeof undefined;
+if (!hasGroup) {
+  const groupBy = (arr, callback) => {
+    return arr.reduce((acc = {}, ...args) => {
+      const key = callback(...args);
+      acc[key] ??= [];
+      acc[key].push(args[0]);
+      return acc;
+    }, {});
+  };
+
+  if (typeof Object.groupBy === typeof undefined) {
+    Object.groupBy = groupBy;
+  }
+}
+
 export default {
   data: {
     title: "TV-Duellen",

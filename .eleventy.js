9524db3db36ce494fd250f466cdb3a0ef6b3bdfd
Maximilian Rågmo <max.ragmo@outlook.com>
2024-10-13 15:54:31 +0200

    Watch files in program folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles/**/*.css");
   eleventyConfig.addPassthroughCopy("program/**/*.jpg");
   eleventyConfig.addPassthroughCopy("library/animation.js");
+  eleventyConfig.setInputDirectory("eleventy");
+  eleventyConfig.addWatchTarget("./program/**/*.js");
 }

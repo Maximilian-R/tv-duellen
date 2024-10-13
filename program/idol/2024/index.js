b056d0abf8b59570de2278b3f6c0ba2ad5e927bc
Maximilian Rågmo <max.ragmo@outlook.com>
2024-10-13 15:00:59 +0200

    Update index.js

--- program/idol/2024/index.js
+++ program/idol/2024/index.js
@@ -3,21 +3,21 @@ import { Game } from "../../../library/game.js";
 const game = new Game(
   { name: "Idol", year: 2024 },
   ["idol", "rounded", "theme-white"],
-  ["✨", "🎙️", "🎶"],
+  ["✨", "🎙️", "🎶"]
 );
 game.contestant("Benjamin");
-game.contestant("Ella");
+game.contestant("Ella").bet("MR");
 game.contestant("Joel");
-game.contestant("Leo");
-game.contestant("Leon");
+game.contestant("Leo").bet("AR");
+game.contestant("Leon").eliminate();
 game.contestant("Lucas");
-game.contestant("Lukas");
+game.contestant("Lukas").bet("FL");
 game.contestant("Margaux");
 game.contestant("Minou");
 game.contestant("Olivia");
-game.contestant("Paulina");
+game.contestant("Paulina").bet("RS");
 game.contestant("Robin H");
-game.contestant("Robin S");
+game.contestant("Robin S").bet("OR").bet("ER");
 game.contestant("Victoria");
 
 export { game };

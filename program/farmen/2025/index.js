3ad2a47f278fed2c6dfaead4786bac2ca7fb1155
Maximilian Rågmo <max.ragmo@outlook.com>
2025-02-26 22:30:01 +0100

    Update index.js

--- program/farmen/2025/index.js
+++ program/farmen/2025/index.js
@@ -6,19 +6,19 @@ const game = new Game(
   ["🥕", "🌽", "🥔"]
 );
 game.contestant("Jonna").quit();
-game.contestant("Sofia");
-game.contestant("Alida");
-game.contestant("Jesper");
+game.contestant("Sofia").bet("AR", false).bet("OR");
+game.contestant("Alida").bet("OR", false);
+game.contestant("Jesper").bet("MR", false);
 game.contestant("Peter").quit();
 game.contestant("Hanna").eliminate();
 game.contestant("Andreas");
-game.contestant("Marica");
+game.contestant("Marica").bet("JR").bet("RS");
 game.contestant("Amanda").eliminate();
-game.contestant("Adam");
-game.contestant("Robert");
+game.contestant("Adam").bet("MR");
+game.contestant("Robert").bet("RS", false);
 game.contestant("Jessica").eliminate();
 game.contestant("Mikael");
-game.contestant("Niklas");
+game.contestant("Niklas").bet("AR").bet("JR", false);
 game.contestant("Graham").quit();
 game.contestant("Nathalie").quit();
 

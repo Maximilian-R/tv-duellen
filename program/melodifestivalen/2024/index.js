190418331a533854f61142d0e2c767fc86a9d57d
Maximilian Rågmo <max.ragmo@outlook.com>
2025-03-08 22:35:41 +0100

    Update index.js

--- program/melodifestivalen/2024/index.js
+++ program/melodifestivalen/2024/index.js
@@ -10,26 +10,26 @@ const game = new Game(
   ["melodifestivalen", "rounded", "theme-white"],
   ["🪩", "🎈", "🎤"]
 );
-game.contestant("Lisa Ajax").eliminate();
-game.contestant("Cazzi Opeia").eliminate();
-game.contestant("Jacqline").bet("OR").bet("JR", false).eliminate();
-game.contestant("Liamoo").eliminate();
-game.contestant("Maria Sur").eliminate();
-game.contestant("Smash Into Pieces").bet("AMS", false).eliminate();
-game.contestant("Danny Saucedo").bet("LR", false).bet("AR", false).eliminate();
-game.contestant("Dotter").eliminate();
+game.contestant("Lisa Ajax").eliminate(11);
+game.contestant("Cazzi Opeia").eliminate(4);
+game.contestant("Jacqline").bet("OR").bet("JR", false).eliminate(9);
+game.contestant("Liamoo").eliminate(5);
+game.contestant("Maria Sur").eliminate(7);
+game.contestant("Smash Into Pieces").bet("AMS", false).eliminate(3);
+game.contestant("Danny Saucedo").bet("LR", false).bet("AR", false).eliminate(6);
+game.contestant("Dotter").eliminate(12);
 game
   .contestant("Jay Smith")
   .bet("RS", false)
   .bet("AMS")
   .bet("EMS", false)
-  .eliminate();
+  .eliminate(10);
 game
   .contestant("Annika Wickihalder")
   .bet("RS")
   .bet("MR")
   .bet("EMS")
-  .eliminate();
+  .eliminate(8);
 game
   .contestant("Marcus & Martinus")
   .bet("JR")
@@ -37,6 +37,6 @@ game
   .bet("AR")
   .bet("OR", false)
   .win();
-game.contestant("Medina").bet("MR", false).eliminate();
+game.contestant("Medina").bet("MR", false).eliminate(2);
 
 export { game };

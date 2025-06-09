0b8166b9a7db5268c38a9d54417d7ff1b5d552ea
Maximilian Rågmo <max.ragmo@outlook.com>
2025-06-09 19:38:59 +0200

    Update index.js

--- program/robinson/2025-1/index.js
+++ program/robinson/2025-1/index.js
@@ -10,30 +10,30 @@ game.contestant("Clara").quit();
 game.contestant("Josefine").eliminate();
 game.contestant("Andreas E").quit();
 game.contestant("Anna").quit();
-game.contestant("Simon H");
+game.contestant("Simon H").eliminate();
 game.contestant("Simon S").eliminate();
 game.contestant("Zuzana").eliminate();
 game.contestant("Angelica").quit();
-game.contestant("Kais");
-game.contestant("Aron").bet("MR", false);
+game.contestant("Kais").eliminate();
+game.contestant("Aron").bet("MR", false).win();
 
 game.contestant("Andrea").eliminate();
 game.contestant("Christoffer").bet("JR", false).eliminate();
 game.contestant("Leith").quit();
 game.contestant("Toni").eliminate();
 game.contestant("David").bet("RS").eliminate();
-game.contestant("Juliette");
+game.contestant("Juliette").eliminate();
 game.contestant("Madeleine").eliminate();
 game.contestant("Amanda Å").quit();
 game.contestant("Andreas L").quit();
 game.contestant("Sewit").eliminate();
 
-game.contestant("Sara");
+game.contestant("Sara").eliminate();
 game.contestant("Amanda E").eliminate();
 game.contestant("Pascal").quit();
 game.contestant("Pål").bet("AR", false).bet("RS", false).bet("MR").eliminate();
 game.contestant("Fabian").quit();
-game.contestant("Sophie").bet("AR").bet("JR").bet("OR");
-game.contestant("Pontus").bet("OR", false);
+game.contestant("Sophie").bet("AR").bet("JR").bet("OR").eliminate();
+game.contestant("Pontus").bet("OR", false).eliminate();
 
 export { game };

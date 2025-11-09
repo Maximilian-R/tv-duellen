8bf7f19b4630271f66ee69a6bcef016f38786597
Maximilian Rågmo <max.ragmo@outlook.com>
2025-11-09 10:00:01 +0100

    Update index.js

--- program/idol/2025/index.js
+++ program/idol/2025/index.js
@@ -5,20 +5,20 @@ const game = new Game(
   ["idol", "rounded", "theme-white"],
   ["🎙️", "✨", "🎶"]
 );
-game.contestant("Allan").eliminate(12);
-game.contestant("Alma").eliminate(12);
+game.contestant("Allan").eliminate(11);
+game.contestant("Alma").eliminate(11);
 game.contestant("Arvid");
 game.contestant("Ingrid").bet("ER").bet("FL");
-game.contestant("Jénovic").bet("JR");
+game.contestant("Jénovic").bet("JR").eliminate(9);
 game.contestant("Jonas");
-game.contestant("Karl").bet("OR");
+game.contestant("Karl").bet("OR").eliminate(9);
 game.contestant("Love B");
 game.contestant("Love S").bet("MR");
-game.contestant("Malva").eliminate(11);
+game.contestant("Malva").eliminate(10);
 game.contestant("Nicolina");
-game.contestant("Nora").eliminate(12);
-game.contestant("Rasmus").eliminate(12);
-game.contestant("Rebecca").eliminate(11);
+game.contestant("Nora").eliminate(11);
+game.contestant("Rasmus").eliminate(11);
+game.contestant("Rebecca").eliminate(10);
 game.contestant("Sofia").bet("RS");
 game.contestant("Tuva").bet("AR");
 

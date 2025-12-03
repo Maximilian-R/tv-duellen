516df649ec6414e52d3e8a20600dca9f2423d66d
Maximilian Rågmo <max.ragmo@outlook.com>
2025-12-03 19:38:26 +0100

    Update index.js

--- program/robinson/2025-2/index.js
+++ program/robinson/2025-2/index.js
@@ -7,27 +7,32 @@ const game = new Game(
 );
 
 game.contestant("Agust").eliminate();
-game.contestant("Akink");
+game.contestant("Akink").secondary("RS").secondary("MR");
 game.contestant("Ania");
 game.contestant("Ann-Helene").eliminate();
 game.contestant("Anna").eliminate();
 game.contestant("Carin");
 game.contestant("Charlie").eliminate();
 game.contestant("Diana").quit();
-game.contestant("Ebba");
+game.contestant("Ebba").eliminate();
 game.contestant("Emilly");
 game.contestant("Emma");
 game.contestant("Jacob").quit();
 game.contestant("Jasmine").eliminate();
 game.contestant("Johan");
 game.contestant("Kevin").eliminate();
-game.contestant("Matteo");
+game
+  .contestant("Matteo")
+  .primary("RS")
+  .primary("AR")
+  .primary("JR")
+  .primary("OR");
 game.contestant("René").eliminate();
 game.contestant("Salah").quit();
-game.contestant("Samuel");
+game.contestant("Samuel").secondary("AR").primary("MR");
 game.contestant("Susanne").eliminate();
 game.contestant("Ulf").eliminate();
 game.contestant("Lea").eliminate();
-game.contestant("Per");
+game.contestant("Per").secondary("JR").secondary("OR");
 
 export { game };

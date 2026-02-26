62842da66d9545198c154204f51ee3b026ddee64
Maximilian Rågmo <max.ragmo@outlook.com>
2026-02-26 16:28:50 +0100

    Update index.js

--- program/farmen/2026/index.js
+++ program/farmen/2026/index.js
@@ -4,33 +4,39 @@ const game = new Game(
   { name: "Farmen", year: 2026 },
   ["farmen"],
   ["🥕", "🌽", "🥔"],
+  false,
 );
-game.contestant("Ahmed");
-game.contestant("Alexander");
+game.contestant("Ahmed").secondary("MR");
+game.contestant("Alexander").primary("AR");
 game.contestant("Amanda");
 game.contestant("Angelica");
-game.contestant("Ayda").eliminate();
+game.contestant("Ayda");
 game.contestant("Bosse");
-game.contestant("Erik");
-game.contestant("Jenny");
-game.contestant("Jonas").eliminate();
-game.contestant("Kevin").eliminate();
+game.contestant("Erik").primary("JR");
+game.contestant("Jenny").secondary("RS");
+game.contestant("Jonas");
+game.contestant("Kevin");
 game.contestant("Niklas");
 game.contestant("Nils").quit();
 game.contestant("Pim").quit();
-game.contestant("Stephanie");
-game.contestant("Tindra").eliminate();
+game
+  .contestant("Stephanie")
+  .primary("RS")
+  .secondary("AR")
+  .secondary("JR")
+  .primary("MR");
+game.contestant("Tindra");
 game.contestant("Vanessa");
 game.contestant("Hampus").quit();
 game.contestant("Jörgen");
-game.contestant("Nowaleya").eliminate();
+game.contestant("Nowaleya");
 
-// game.eliminate("Tindra");
-// game.eliminate("Ayda");
-// game.eliminate("Jonas");
+game.eliminate("Tindra");
+game.eliminate("Ayda");
+game.eliminate("Jonas");
 // game.quit(["Nils", "Pim"]);
-// game.eliminate("Kevin");
+game.eliminate("Kevin");
 // game.quit("Hampus");
-// game.eliminate("Nowaleya");
+game.eliminate("Nowaleya");
 
 export { game };

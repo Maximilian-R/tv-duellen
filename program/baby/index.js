3a64defe149e28f2d8e88257a4404b92062603a8
Maximilian Rågmo <max.ragmo@outlook.com>
2026-04-08 20:17:34 +0200

    Update index.js

--- program/baby/index.js
+++ program/baby/index.js
@@ -3,7 +3,7 @@ import { Game } from "../../library/game.js";
 export class Baby extends Game {
   constructor(year) {
     super(
-      { name: "Bebis", year },
+      { name: "Bebis", year, secondaryVotes: 0 },
       ["baby", "theme-white"],
       ["🍼", "🍼", "🍼"],
       false,
